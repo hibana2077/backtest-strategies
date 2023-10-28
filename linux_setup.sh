5728447ba568415713bc090635543f859ac69b5b
hibana2077 <hibana2077@gmail.com>
2023-10-28 13:52:33 +0800

    update setup script

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,1 +1,8 @@
 pip3 install pandas_ta ccxt pandas numpy scipy backtesting matplotlib
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+sudo ./configure
+sudo make
+sudo make install
+pip3 install ta-lib
